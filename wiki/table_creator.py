bc87cb1209cfe47fe16b92f16018b2fd14478b75
jaymsherman <sherman.j@husky.neu.edu>
2020-04-02 23:57:38 -0400

    moved mwparser out of class

--- wiki/table_creator.py
+++ wiki/table_creator.py
@@ -21,39 +21,60 @@ def parse_colons(s):
 
 if __name__ == "__main__":
 
+    """#reading in data from index text file, turning into table with columns relating, in order:
+    #byte location of xml tags in .bz2 file, id of page, title of page
     indices = pandas.read_csv(INPUT_DATA_DIR / "enwiki-20200201-pages-articles-multistream-index.txt",
                               delimiter = "|")
     print("done reading from csv")
     indices_values = indices["byte:id:title"]
     row_values = []
+    byte_values = [] #just the byte location of xml tag, first column of row
     for i in range(len(indices_values)):
         if i % 10000 == 0:
             print(i)
         line = indices_values[i]
+        row_value = parse_colons(line)
+        byte_values.append(row_value[0])
         row_values.append(parse_colons(line))
 
-    print("done formatting data")
+    print("Done formatting data")
+    
+    articles = pandas.DataFrame(row_values, columns = ["first_byte", "page_id", "title"])
 
-    #finding the next byte to read to- -1 if it should read to end of file
-    byte_values = list(set([val[0] for val in row_values]))
+    #Creating a mapping from the byte location of the xml tag to the next byte value,
+    #so that only a small range of the bz2 file needs to be decompressed
+    byte_values = articles["first_byte"]
+    byte_values = list(set(byte_values))
     byte_values.sort()
+    byte_values.append(-1) #for articles at the last byte, will read to end of file
     byte_dict = {}
-    for i, byte in enumerate(byte_values):
-        byte_dict[byte] = byte_values[min(len(byte_values) - 1, i + 1)]
-        if byte_dict[byte] == byte:
-            byte_dict[byte] = -1
+    for i in range(len(byte_values) - 1):
+        byte_dict[byte_values[i]] = byte_values[i + 1]
 
-    for row in row_values:
-        row.append(byte_dict[row[0]])
+    print("Byte mapping built")
+    
+    #Creating new row on dataframe to relate the end of partial decompression range
+    last_bytes = [byte_dict[byte] for byte in articles["first_byte"]]
+    articles["last_byte"] = last_bytes
+    
+    print("Byte ranges assigned")
 
-    articles = pandas.DataFrame(row_values, columns = ["first_byte", "page_id", "title", "last_byte"])
+    #not tracked on repo- just for debug purposes
+    articles.to_csv(OUTPUT_DATA_DIR / "pages.csv")"""
 
-    conn = sqlite3.connect(OUTPUT_DATA_DIR / 'xml_indices.db')
+    #remove
+    articles = pandas.read_csv(OUTPUT_DATA_DIR / "articles.csv")
+    articles = articles.loc[:, ~articles.columns.str.contains('^Unnamed')]
+    print("done reading from csv")
+
+    #creation of sqlite table
+    conn = sqlite3.connect(OUTPUT_DATA_DIR / 'pages.db') #also not tracked on repo
+    print("found connection")
     cursor = conn.cursor()
-    cursor.execute("DROP TABLE IF EXISTS articles")
-    cursor.execute('CREATE TABLE articles (first_byte, page_id, title, last_byte)')
-    cursor.execute("CREATE INDEX articles_id_idx ON articles (page_id)")
-    cursor.execute("CREATE INDEX articles_title_idx ON articles (title)")
-    articles.to_sql("articles", conn)
+    cursor.execute("DROP TABLE IF EXISTS pages")
+    articles.to_sql("pages", conn, index = False)
+    print("read out to sql")
+    cursor.execute("CREATE INDEX articles_id_idx ON pages (page_id)")
+    cursor.execute("CREATE INDEX articles_title_idx ON pages (title)")
     conn.commit()
     conn.close()
