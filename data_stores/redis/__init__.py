98da5f1ddde24f9b688470cc6942a0356b8a1823
maxc1118 <maxcunha11@gmail.com>
2020-04-04 19:17:45 -0400

    attempting to fix private email commit error

--- data_stores/redis/__init__.py
+++ data_stores/redis/__init__.py
@@ -1,7 +1,7 @@
 """
 Initialization of Redis interfaces and project-specific methods.
 
-Written by Max Cunha @maxc1118.
+Written by Max Cunha.
 """
 from data_stores.redis.article_cache import ArticleCache
 
