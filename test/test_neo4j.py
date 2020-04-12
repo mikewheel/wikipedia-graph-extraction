cfaf867d2574601625bb98c17cd462e9fa68cbdf
Michael Wheeler <wheeler.m@husky.neu.edu>
2020-04-12 14:50:28 -0400

    Removed stale TODOs

--- test/test_neo4j.py
+++ test/test_neo4j.py
@@ -3,8 +3,6 @@ from config import NEO4J_CONNECTION_PARAMETERS
 from wikipedia.models import WikipediaArticle
 
 if __name__ == "__main__":
-    # TODO -- init connection to Neo4J using NEO4J_CONNECTION_PARAMETERS
-    # TODO -- iterate over SEED_LIST: if not in data store, add_node
     print("config", NEO4J_CONNECTION_PARAMETERS)
     src = WikipediaArticle(article_title="cool article 1", article_url="url 1", index_key="pura vida")
     dest = WikipediaArticle(article_title="lame article", article_url="url 2", index_key="swag")
